1b588c3190f763e6c5c61830ed6c5eb050d54d41
ArifIlham0 <marifilham999@gmail.com>
2024-02-11 22:02:13 +0700

    menghapus nilai umur

--- bin/input_output.dart
+++ bin/input_output.dart
@@ -4,11 +4,11 @@ void main() {
   // Menggunakan fungsi write dari class stdout yang diambil dari package dart io
   stdout.write("Masukkan nama anda: ");
   var nama = stdin.readLineSync();
-  stdout.write("Masukkan umur anda: ");
-  var umur = stdin.readLineSync();
+  // stdout.write("Masukkan umur anda: ");
+  // var umur = stdin.readLineSync();
 
   //Diubah dari string ke int, karna inputannya berupa angka.
-  var umurInt = int.parse('$umur');
+  // var umurInt = int.parse('$umur');
 
   print("-------------------------");
 
